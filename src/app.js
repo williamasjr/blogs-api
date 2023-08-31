b1e270c32b36d7b316bcb365deb6947935b1ce07
williamasjr <wasjr00@gmail.com>
2023-08-30 23:51:25 -0300

    organize and delete files

--- src/app.js
+++ src/app.js
@@ -1,20 +1,13 @@
 const express = require('express');
 const { loginRouter, userRouter, categoriesRouter, postRouter } = require('./routes');
-// ...
 
 const app = express();
 
-// não remova ou mova esse endpoint
 app.get('/', (_request, response) => {
   response.send();
 });
 
 app.use(express.json());
-
-// ...
-
-// É importante exportar a constante `app`,
-// para que possa ser utilizada pelo arquivo `src/server.js`
 
 app.use('/', loginRouter);
 
